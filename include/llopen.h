f430ba94bd1f4a3a2077c94107202c7f450a996d
ent <ent@ent-ROG-Zephyrus-G14-GA401QM-PX401QM.home>
2024-04-28 22:55:47 +0100

    llopen complete?

--- include/llopen.h
+++ include/llopen.h
@@ -1,23 +1,22 @@
 #ifndef LLOPEN
 #define LLOPEN
 
-#include <termios.h>
 #include "linklayer.h"
+#include "fcntl.h"
 
 #define ack_message_length 5
 #define buff_size 256
 
+void set_termios(int fd, struct termios* tio, struct linkLayer params);
 
-void set_termios(struct termios* tio, struct linkLayer params);
+void* create_conn_message(enum SIGNALS type, int role, char* buff, int length);
 
-void* create_conn_message(enum SIGNALS type, int role, char* buff);
+int send_message(int fd, enum SIGNALS type, char* buff, int legnth, int role, struct flock* lock);
 
-int send_message(int fd, enum SIGNALS type, char* buff, int role);
+int read_buffer(int fd, char* buff, int length, struct flock* lock);
 
-void read_buffer(int fd, char* buff, int length);
+int check_for_UA(int fd, char *buff, int length, struct flock* lock);
 
-int check_for_UA(int fd, char *buff, int length);
-
-int check_for_SET(int fd, char *buff, int length);
+int check_for_SET(int fd, char *buff, int length, struct flock* lock);
 
 #endif
--- src/llopen.c
+++ src/llopen.c
@@ -8,7 +8,7 @@ int llopen(linkLayer connectionParameters)
     bzero(&newtio, sizeof(newtio));
 
     char* s_port = connectionParameters.serialPort;
-    fd = open(s_port, O_RDWR | O_NOCTTY );
+    fd = open(s_port, O_RDWR | O_NOCTTY);
     if (fd < 0)
     {
         perror(s_port);
@@ -21,42 +21,52 @@ int llopen(linkLayer connectionParameters)
         exit(-1);
     }
 
-    set_termios(&newtio, connectionParameters);
+    set_termios(fd, &newtio, connectionParameters);
 
-    tcflush(fd, TCIOFLUSH);         /* Flush the fd to get rid of old data */
-
+    tcflush(fd, TCIOFLUSH);        /* Flush the fd to get rid of old data */
 
     printf("New termios structure set\n");
-
     int role = connectionParameters.role;
     char buff[ack_message_length];
+    bzero(buff, ack_message_length);
+
+    struct flock lock;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = 0;
+    lock.l_len = 0;
+    lock.l_pid = getpid();
 
     if(!role)
     {   
+        printf("Transmitter!\n");
         int res = 0;
         for (int i = 0; i < connectionParameters.numTries; i++)
         {
-            res = send_message(fd, SET, buff, role);
+            res = send_message(fd, SET, buff, ack_message_length, role, &lock);
             usleep(connectionParameters.timeOut*1000);
-            if(check_for_UA(fd, buff, ack_message_length)) break; 
+            printf("buff after send - %x%x%x%x\n", buff[0], buff[1], buff[2], buff[3]);
+            if(check_for_UA(fd, buff, ack_message_length, &lock)) break;
         }
+        printf("buff after read - %x%x%x%x\n", buff[0], buff[1], buff[2], buff[3]);
     }
     else
     {
+        printf("Receiver!\n");
         int res = 0;
-        for (int i = 0; i < connectionParameters.numTries + 10; i++)
+        for (int i = 0; ; i++)
         {
             usleep(connectionParameters.timeOut*1000);
-            printf("ada\n");
-            if(check_for_SET(fd, buff, ack_message_length)) {printf("ada\n"); printf("%s", buff); break;}
+            if(check_for_SET(fd, buff, ack_message_length, &lock)) break;
         }
-        res = send_message(fd, UA, buff, role);
+        printf("buff after read - %x%x%x%x\n", buff[0], buff[1], buff[2], buff[3]);
+        res = send_message(fd, UA, buff, ack_message_length, role, &lock);
+        printf("buff after send - %x%x%x%x\n", buff[0], buff[1], buff[2], buff[3]);
     }
 
     return 1;
 }
 
-void set_termios(struct termios* tio, struct linkLayer params)
+void set_termios(int fd, struct termios* tio, struct linkLayer params)
 {
     tio->c_cflag = params.baudRate | CS8 | CLOCAL | CREAD;
     tio->c_iflag = IGNPAR;
@@ -64,63 +74,83 @@ void set_termios(struct termios* tio, struct linkLayer params)
     tio->c_lflag = 0;                       /* set input mode NON-CANONICAL*/
     tio->c_cc[VTIME] = params.timeOut;      /* inter-character timer unused,  waits 0 seconds between chars*/
     tio->c_cc[VMIN] = params.numTries;      /* blocking read until x chars received */
+
+    if (tcsetattr(fd, TCSANOW, tio) == -1) {perror("tcsetattr"); exit(-1);}
 }
 
-void* create_conn_message(enum SIGNALS type, int role, char* buff)
+void* create_conn_message(enum SIGNALS type, int role, char* buff, int length)
 {
     char addr = role ? RECV_ADDR : TRANS_ADDR;
     char cntrl = type;
     char BCC1 = addr ^ cntrl;
 
-    if(sizeof(buff)/sizeof(buff[0]) >= 5)
+    if(length >= ack_message_length)
     {
+        bzero(buff, ack_message_length);
         buff[0] = FLAG;
         buff[1] = addr;
         buff[2] = cntrl;
         buff[3] = BCC1;
         buff[4] = FLAG;
 
-        printf(" HERE - %x%x%x%x", buff[0], buff[1], buff[2], buff[3]);
+        //printf(" HERE - %x%x%x%x\n", buff[0], buff[1], buff[2], buff[3]);
     }else { perror("create_conn_message"); exit(-1); }
 }
 
-int send_message(int fd, enum SIGNALS type, char* buff, int role)
+int send_message(int fd, enum SIGNALS type, char* buff, int length, int role, struct flock* lock)
 {
-    char response[buff_size];
     int res;
-    buff = create_conn_message(type, role, buff);
+    create_conn_message(type, role, buff, length);
+
+    lock->l_type = F_WRLCK;
+    if(fcntl(fd, F_SETLK, lock) < 0) perror("SEND_LOCK");
+    
+    //printf("b - %c <-\n", buff[2]);
     res = write(fd, buff, ack_message_length);
+    printf("WRITE DONE!\n");
+
+    lock->l_type = F_UNLCK;
+    fcntl(fd, F_SETLK, lock);
 
     return res;
 }
 
-void read_buffer(int fd, char* buff, int length)
+int read_buffer(int fd, char* buff, int length, struct flock* lock)
 {
     int total_read = 0;
+    int res = -1;
+
+    lock->l_type = F_WRLCK;
+    if(fcntl(fd, F_SETLK, lock) < 0) perror("READ_LOCK");
+
     while (total_read < length)
     {
-        int res = read(fd, buff + total_read, length - total_read); //only read length chars
+        res = read(fd, buff + total_read, length - total_read); //only reasd length chars
+        printf("READ DONE!\n");
+        //printf("%d\n", res);
         if (res < 0) {perror("read"); break;}
-        //else if (res == 0) break; //EOF
         total_read += res;
     }
-    buff[total_read] = '\0'; // Null-terminate the buffer
+
+    //if (ftruncate(fd, 0) < 0) perror("ftruncate");  // wipe the file --> tcflush?
+
+    lock->l_type = F_UNLCK;
+    fcntl(fd, F_SETLK, lock);
+    
+    buff[total_read] = '\0';
+    return res;
 }
 
-int check_for_UA(int fd, char *buff, int length)
+int check_for_UA(int fd, char *buff, int length, struct flock* lock)
 {
-    bzero(buff, length);
-    read_buffer(fd, buff, length);
-    printf("%x", buff[2]);
-    if(buff[2] == 0x06) printf("!#!\n");
+    //bzero(buff, length);
+    read_buffer(fd, buff, length, lock);
     return buff[2] == 0x06;
 }
-
-int check_for_SET(int fd, char *buff, int length)
+ 
+int check_for_SET(int fd, char *buff, int length, struct flock* lock)
 {
-    bzero(buff, length);
-    read_buffer(fd, buff, length);
-    printf("%x", buff[2]);
-    if(buff[2] == 0x08) printf("!#!dad\n");
+    //bzero(buff, length);
+    read_buffer(fd, buff, length, lock);
     return buff[2] == 0x08;
 }
